eba985f7926e827513cdfa1250ed11c2c26a1f09
169398 <kulubiidris@gmail.com>
2024-04-30 00:57:33 +0300

    first commit

---
+++ src/components/Header/Header.data.ts
@@ -0,0 +1,22 @@
+export const dataHeader = [
+  {
+    id: 1,
+    name: "Features",
+    idLink: "#features",
+  },
+  {
+    id: 2,
+    name: "Targets",
+    idLink: "#tarjetas",
+  },
+  {
+    id: 3,
+    name: "Testimonials",
+    idLink: "#testimonios",
+  },
+  {
+    id: 4,
+    name: "Services",
+    idLink: "#servicios",
+  },
+];
